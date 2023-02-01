ea480b25fdcf16db2f19a4cda207ef70040b45c2
urc4 <121357043+urc4@users.noreply.github.com>
2023-02-01 09:14:26 -0300

    Paint grid
    This introduces some new functionalities to the grid to be able to
    paint it dynamically using an external color variable from js to css
    with the var satement. Now, we need to be able to choose between
    painting each pixel individually or click and drag until you click
    again. Gotta have a color pallete to choose as well as the rainbow
    mode and the erase mode.

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ function createGridBlock(gridDivison) {
     LENGTH / gridDivison
   }px;`;
   gridBlock.style.cssText = gridStyle;
-  gridBlock.classList.toggle("grid-block-active");
+  gridBlock.classList.toggle("grid-block");
+  gridBlock.classList.toggle("inactive");
   GRID.appendChild(gridBlock);
 }
 // If you input an invalid command the browser just goes crazy trying to look for it
@@ -26,14 +27,42 @@ function createGrid(gridDivision) {
   paintGrid();
 }
 createGrid(32);
+//if you leave a function with an empty statement it just does not stop runnnig
+// or should it return undefined??
+
+function getColor() {
+  //has to be a string
+
+  return "green";
+  //add eventlistners to buttons later on
+}
+
+function isPainted(gridBlock) {
+  const painted = gridBlock.classList.contains("active");
+  if (painted) return true;
+  return false;
+}
+
+function paintBlock(block) {
+  const color = getColor();
+  block.style.setProperty("--grid-block-color", color);
+
+  if (isPainted(block)) return;
+
+  block.classList.toggle("inactive");
+  block.classList.toggle("active");
+
+  //create a current color variable
+
+  // block.style.backgroundColor = "yellow";
+  // block.style.cssText = "background-color:pink;"; why does it not work inline js has higher specificty
+}
 
 function paintGrid() {
-  const gridBlocks = document.querySelectorAll(".grid-block-active");
+  const gridBlocks = document.querySelectorAll(".grid-block");
   gridBlocks.forEach((block) => {
-    block.addEventListener("mousedown", () => {
-      block.classList.toggle("grid-block-active");
-      block.classList.toggle("grid-block-painted");
-    });
+    block.addEventListener("mousedown", () => paintBlock(block));
+    //add mouse down and mouse over and click functionalities to paint individually and drag
   });
 }
 

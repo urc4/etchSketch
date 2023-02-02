f4986b3e08ad2eda789da599df99dc3f826ab58b
urc4 <121357043+urc4@users.noreply.github.com>
2023-02-02 16:41:41 -0300

    Take a step back so we can take two forward
    .

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function removeGrid() {
 
 function eraseBlock(block) {
   if (!isPainted(block)) return;
-  block.classList.remove("active"); //toggle gets confusing with lots of elemetns
+  block.classList.remove("active");
   block.classList.add("inactive");
 }
 
@@ -104,15 +104,12 @@ function isPainted(gridBlock) {
 }
 
 function paintBlock(block, color) {
-  // const color = getColor();
   block.style.setProperty("--grid-block-color", color);
 
   if (isPainted(block)) return;
 
   block.classList.remove("inactive");
   block.classList.add("active");
-
-  //create a current color variable
 }
 
 function toggleButtons(btnSelected) {
@@ -137,69 +134,12 @@ function selectButton(btnSelected, showNext) {
 }
 
 function showNextClickDrag() {
-  const clickDragButton = document.querySelector(".click-drag-btn");
-  const clickDrag = clickDragButton.id;
-  const textDiv = clickDragButton.querySelector(".text-btn");
-  clickDragButton.removeAttribute("id");
-  let nextClickDrag = clickDrag;
-  if (clickDrag === "click") nextClickDrag = "drag";
-  else nextClickDrag = "click";
-
-  clickDragButton.setAttribute("id", `${nextClickDrag}`);
-  textDiv.textContent = `${nextClickDrag}`;
-}
-
-function makeClickOrDrag(choice) {
-  // const choice = document.querySelector(".click-drag-btn").id;
-  const gridBlocks = document.querySelectorAll(".grid-block");
-
-  if (choice === "click") {
-    clickPaint(gridBlocks);
-  } else if (choice === "drag") {
-    dragPaint(gridBlocks);
-  }
-}
-
-function dragPaint(gridBlocks) {
-  let toggle = true;
-  gridBlocks.forEach((block) => {
-    block.addEventListener("mousedown", () => {
-      toggle = !toggle;
-      if (toggle) {
-        block.addEventListener("mouseover", () => {
-          const color = getColor();
-          paintBlock(block, color);
-        });
-      }
-    });
-    //add mouse down and mouse over and click functionalities to paint individually and drag
-  });
-}
-
-function clickPaint(gridBlocks) {
-  gridBlocks.forEach((block) => {
-    block.addEventListener("mousedown", () => {
-      const color = getColor();
-      paintBlock(block, color);
-    });
-    //add mouse down and mouse over and click functionalities to paint individually and drag
-  });
-}
-
-function createClickDragButton() {
-  const clickDragButton = document.querySelector(".grid-division-btn");
-  clickDragButton.addEventListener("click", () => {
-    selectButton(clickDragButton, showNextClickDrag);
-    makeClickOrDrag(clickDragButton.id);
-  });
-}
-
-function showNextGridDivision() {
   const gridButton = document.querySelector(".grid-division-btn");
-  const clickDrag = Number(gridButton.id);
+  const gridDivision = Number(gridButton.id);
   const textDiv = gridButton.querySelector(".text-btn");
   gridButton.removeAttribute("id");
-  let nextDivision = clickDrag;
+  let nextDivision = gridDivision;
+  console.log(gridButton.id);
   if (nextDivision + 16 > 100) nextDivision = 16;
   else nextDivision += 16;
 
@@ -207,8 +147,30 @@ function showNextGridDivision() {
   textDiv.textContent = `${nextDivision}`;
 }
 
-//can make this fucntion take two argumetns gridBlocks and btnSelected and then new function to go
-//isnide forEach
+function creteClickDragButton() {
+  const gridButton = document.querySelector(".grid-division-btn");
+  gridButton.addEventListener("click", () => {
+    const textDiv = gridButton.querySelector(".text-btn");
+    textDiv.textContent = gridButton.id;
+    selectButton(gridButton, showNextGridDivision);
+    createGrid(Number(gridButton.id));
+  });
+}
+
+function showNextGridDivision() {
+  const gridButton = document.querySelector(".grid-division-btn");
+  const gridDivision = Number(gridButton.id);
+  const textDiv = gridButton.querySelector(".text-btn");
+  gridButton.removeAttribute("id");
+  let nextDivision = gridDivision;
+  console.log(gridButton.id);
+  if (nextDivision + 16 > 100) nextDivision = 16;
+  else nextDivision += 16;
+
+  gridButton.setAttribute("id", `${nextDivision}`);
+  textDiv.textContent = `${nextDivision}`;
+}
+
 function createGridButton() {
   const gridButton = document.querySelector(".grid-division-btn");
   gridButton.addEventListener("click", () => {
@@ -230,9 +192,14 @@ function displayClearGridButton() {
 function paintGridButton() {
   const colorButton = document.querySelector(".color-btn");
   colorButton.addEventListener("click", () => {
-    makeClickOrDrag();
-
-    selectButton(colorButton, showNextColor); //if i put this one line above it just gets so freaknig random
+    const gridBlocks = document.querySelectorAll(".grid-block");
+    gridBlocks.forEach((block) => {
+      block.addEventListener("mousedown", () => {
+        const color = getColor();
+        paintBlock(block, color);
+      });
+    });
+    selectButton(colorButton, showNextColor);
     displayClearGridButton();
   });
 }
@@ -247,9 +214,8 @@ function paintRandomButton() {
         color = getRandomColor();
         paintBlock(block, color);
       });
-      //add mouse down and mouse over and click functionalities to paint individually and drag
     });
-    selectButton(rainbowButton, () => {}); //if i put this one line above it just gets so freaknig random
+    selectButton(rainbowButton, () => {});
     displayClearGridButton();
   });
 }
@@ -259,22 +225,14 @@ function eraseGridButton() {
   eraserButton.addEventListener("click", () => {
     const gridBlocks = document.querySelectorAll(".grid-block");
     gridBlocks.forEach((block) => {
-      block.addEventListener("mousedown", () => eraseBlock(block)); //just add an if statment here to toggle options
-      //add mouse down and mouse over and click functionalities to paint individually and drag
+      block.addEventListener("mousedown", () => eraseBlock(block));
     });
     selectButton(eraserButton, () => {});
     displayClearGridButton();
   });
 }
-
 createGrid(32);
-// let gridBlocks = document.querySelectorAll(".grid-block");
 paintGridButton();
 eraseGridButton();
 createGridButton();
 paintRandomButton();
-createClickDragButton();
-// if ((gridBlocks = createGridButton())) {
-//   paintGridButton();
-//   eraseGridButton();
-// }
